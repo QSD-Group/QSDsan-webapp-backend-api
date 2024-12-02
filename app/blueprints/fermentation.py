a90683bf64212c52e87320b7dbc2314f78202a63
Naman Shah <80583973+navronaman@users.noreply.github.com>
2024-12-01 22:24:49 -0500

    Add fermentation data file and update API endpoints for calculations and county data

--- app/blueprints/fermentation.py
+++ app/blueprints/fermentation.py
@@ -1,13 +1,200 @@
 from flask import Blueprint, request, jsonify, make_response
-from app.services.fermentation_service import fermentation_calc, fermentation_county, fermentation_convert_mass_kg
+from app.services.fermentation_service import fermentation_calc, fermentation_county, fermentation_kg
 
 fermentation_bp = Blueprint('fermentation_bp', __name__)
 
-@fermentation_bp.route('/fermentation/calc', methods=['POST'])
-def fermentation_calc():
-    None
+@fermentation_bp.route('/fermentation/calc', methods=['GET'])
+def fermentation_calc_data():
+    """
+    Takes in a mass amount in a specified unit and returns the (i) mass of the feedstock in kg/hr, (ii) ethanol produced in MM gallons/year, (iii) price of ethanol in $/gallon, (iv) greenhouse gas emissions in lb CO2e/gallon.
+    
+    URL Format - GET /fermentation/calc?mass=mass&unit=unit
+    ---
+    tags:
+      - Fermentation
+    parameters:
+      - name: mass
+          in: query
+          type: number
+          format: float
+          required: true
+          description: The mass of the feed
+          example: 100.0
+    - name: unit
+      in: query
+      type: string
+      required: true
+      description: The unit of the mass
+      example: 'kghr, tons, tonnes'
+    responses:
+      200:
+        description: A successful response
+        content:
+          application/json:
+            schema:
+              type: object
+              properties:
+                mass:
+                  type: number
+                  format: float
+                  example: 100.0
+                  description: The mass of the feedstock in kg/hr
+                ethanol:
+                  type: number
+                  format: float
+                  example: 0.0
+                  description: The ethanol produced in MM gallons/year
+                price:
+                  type: number
+                  format: float
+                  example: 0.0
+                  description: The price of ethanol in $/gallon
+                gwp:
+                  type: number
+                  format: float
+                  example: 0.0
+                  description: The GWP in lb CO2e/gallon
+      400:
+        description: Bad request
+        content:
+          application/json:
+            schema:
+              type: object
+              properties:
+                message:
+                  type: string
+                  example: 'Mass is required'
+      422:
+        description: Unprocessable entity
+        content:
+          application/json:
+            schema:
+              type: object
+              properties:
+                message:
+                  type: string
+                  example: 'Unit must be one of 'kghr', 'tons', 'tonnes''
+      500:
+        description: Unknown error
+        content:
+          application/json:
+            schema:
+              type: object
+              properties:
+                message:
+                  type: string
+                  example: 'An unknown error occurred'
+    """
+    mass = request.args.get('mass', None)
+    
+    if mass is None:
+        return make_response(
+            jsonify({"error": "Mass is required"}), 400 # Bad request
+        )
+    
+    try:
+        mass = float(mass)
+    except ValueError:
+        return make_response(
+            jsonify({"error": "Mass must be a number"}), 400 # Bad request
+        )
+    
+    unit = request.args.get('unit', 'kghr')
+    if unit not in ['kghr', 'tons', 'tonnes']:
+        return make_response(
+            jsonify({"error": "Unit must be one of 'kghr', 'tons', 'tonnes'"}), 422 # Unprocessable entity
+        )
+    
+    try:
+        kg_hr = fermentation_kg(mass, unit)
+        ethanol, price, gwp = fermentation_calc(kg_hr)
+        response_data = {
+            "mass": kg_hr, # In kg/hr
+            "ethanol": ethanol, # In MM gallons/year
+            "price": price, # In $/gallon
+            "gwp": gwp # In lb CO2e/gallon
+        }
+        return make_response(
+            jsonify(response_data), 200 # Returns a success status code
+        )
+    except Exception as e:
+        return make_response(
+            jsonify({"error": str(e)}), 500 # Unknown error
+        )
+        
     
 @fermentation_bp.route('/fermentation/county', methods=['GET'])
-def fermentation_county():
-    None
+def fermentation_county_data():
+    """
+    Takes in a county name and returns the fermentation conversion efficiency for that county.
+    
+    URL Format - GET /fermentation/county?county_name=county_name
+    ---
+    tags:
+      - Fermentation
+    parameters:
+      - name: county_name
+        in: query
+        type: string
+        required: true
+        description: The name of the county'
+        example: 'Atlantic'
+    responses:
+      200:
+        description: A successful response
+        content:
+          application/json:
+            schema:
+              type: object
+              properties:
+                county_name:
+                  type: string
+                  example: 'Atlantic'
+                mass:
+                  type: number
+                  format: float
+                  example: 0.0
+                  description: The mass of the feedstock in kg/hr
+                ethanol:
+                  type: number
+                  format: float    
+                  example: 0.0
+                  description: The ethanol produced in MM gallons/year
+                price:
+                  type: float
+                  example: 0.0
+                gwp:
+                  type: float
+                  example: 0.0
+      400:
+        description: Bad request
+        content:
+          application/json:
+            schema:
+              type: object
+              properties:
+                message:
+                  type: string
+                  example: 'Bad request'
+      404:
+        description: Not found
+        content:
+          application/json:
+            schema:
+              type: object
+              properties:
+                message:
+                  type: string
+                  example: 'County not found'
+      500:
+        description: Unexpected error
+        content:
+          application/json:
+            schema:
+              type: object
+              properties:
+                message:
+                  type: string
+                  example: 'Unexpected error'
+    """
     
--- app/services/fermentation_service.py
+++ app/services/fermentation_service.py
@@ -1,3 +1,18 @@
+"""
+What dpes this file do?
+
+It creates three important functions that will be called throughout the project:
+1. fermentation_convert_feedstock_kg_hr: This function converts the feedstock to kg/hr
+2. fermentation_calc: This function calculates the (i) annual ethanol in MM gal/year, (ii) price in $/gal and (iii) GWP in lbCO2/yr based on the given mass of ethanol produced
+3. fermentation_county: This function takes a county name from the user and returns the (i) annual ethanol, (ii) price and (iii) GWP
+
+Where are these functions used in the project?
+1. fermentation.py - Blueprint for Flask
+
+What does the file rely on?
+1. biosteam, biorefineries, pandas, warnings
+2. biomass_data.csv
+"""
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
@@ -32,12 +47,60 @@ GWP_CFs = {
     'FGD_lime': 1, #!!! need to be clear if this is CaO or Ca(OH)2
     }
 
-STATE_DATA = pd.read_csv(r'backend\fermentation\biomass_data.csv')
-STATE_DATA["Kilogram"] = STATE_DATA["Lignocellulose (dry tons)"] * 907.185
-STATE_DATA["Kilogram/hr"] = STATE_DATA["Kilogram"] / (365*24*0.96)
+STATE_DATA = pd.read_csv(r"app\data\fermentation\fermentation_data.csv")
+
+def fermentation_convert_feedstock_kg_hr(feedstock, unit='kghr'):
+    """
+    Convert the feedstock to kg/hr.
+    
+    Parameters
+    ----------
+    feedstock : float
+        The mass of the feed
+    unit : str
+        The unit of the feedstock mass.
+        They can choose from the following:
+        - 'kghr' kg/hr
+        - 'tons' tons/yr
+        - 'tonnes' tonnes/yr
+        Default is 'kghr'.
+    Returns
+    -------
+    feedstock : float
+        The mass of the feedstock in kg/hr.
+        
+    Raises
+    ------
+    ValueError
+        If the unit is not in the list of valid units.
+    TypeError
+        If the feedstock is not a number.
+    TypeError
+        If the unit is not a string.
+        
+    Example
+    -------
+    >>> fermentation_convert_feedstock_kg_hr(100, 'tons')
+    0.011363636363636364
+    """
+    
+    # Type checks
+    if not isinstance(feedstock, (int, float)):
+        raise TypeError('Feedstock should be a number')
+    if not isinstance(unit, str):
+        raise TypeError('Unit should be a string')
+    
+    if unit.lower() == 'kghr':
+        return feedstock # No conversion needed
+    elif unit.lower() == 'tons':
+        return feedstock * 907.185 / 8760 # Convert tons/yr to kg/hr
+    elif unit.lower() == 'tonnes':
+        return feedstock * 1000 / 8760 # Convert tonnes/yr to kg/hr
+    else:
+        raise ValueError('Invalid unit')
 
 
-def lignocellulose_calc(mass, cornstover_price=0.2, GWP_CFs=GWP_CFs, characterization_factors=(1., 1.,), power_utility_price=0.07):
+def fermentation_calc(mass, cornstover_price=0.2, GWP_CFs=GWP_CFs, characterization_factors=(1., 1.,), power_utility_price=0.07):
     """
     Calculate the annual ethanol price and GWP based on the given mass of ethanol produced.
     
@@ -67,7 +130,42 @@ def lignocellulose_calc(mass, cornstover_price=0.2, GWP_CFs=GWP_CFs, characteriz
     power_utility_price : float
         Price of power utility (USD/kWh).
         Set to default value of 0.07.
+        
+    Returns
+    -------
+    tuple
+        Contains the following:
+        - annual ethanol (MM gal/yr)
+        - price ($/gal)
+        - GWP (lb CO2e/gal)
+    Raises
+    ------
+    TypeError
+        If mass is not a number.
+    TypeError
+        If cornstover_price is not a number.
+    TypeError
+        If power_utility_price is not a number.
+    TypeError
+        If GWP_CFs is not a dictionary.
+    TypeError
+        If characterization_factors is not a tuple.
+    TypeError
+        If the power utility price is not a number.
+    Note: Add more checks for the other parameters.
     """
+    
+    # Type checks
+    if not isinstance(mass, (int, float)):
+        raise TypeError('Mass should be a number')
+    if not isinstance(cornstover_price, (int, float)):
+        raise TypeError('Cornstover price should be a number')
+    if not isinstance(power_utility_price, (int, float)):
+        raise TypeError('Power utility price should be a number')
+    if not isinstance(GWP_CFs, dict):
+        raise TypeError('GWP_CFs should be a dictionary')
+    if not isinstance(characterization_factors, tuple):
+        raise TypeError('Characterization factors should be a tuple')
     
     br = CellulosicEthanol(
         name='ethanol',
@@ -98,14 +196,15 @@ def lignocellulose_calc(mass, cornstover_price=0.2, GWP_CFs=GWP_CFs, characteriz
     get_MESP = lambda: tea.solve_price(ethanol)*ethanol_density_kggal # from $/kg to $/gallon
     get_GWP = lambda: (sys.get_net_impact('GWP')/sys.operating_hours/ethanol.F_mass*ethanol_density_kggal)*kg_to_lb_conversion_factor # lb CO2e/gal
 
-    print(f'annual ethanol: ${get_ethanol():.3f} MM gal/yr')
-    print(f'price: ${get_MESP():.2f}/gal')
-    print(f'GWP: {get_GWP():.2f} lb CO2e/gal')
+    # Comment these print statements for now
+    # print(f'annual ethanol: ${get_ethanol():.3f} MM gal/yr')
+    # print(f'price: ${get_MESP():.2f}/gal')
+    # print(f'GWP: {get_GWP():.2f} lb CO2e/gal')
     
-    return get_ethanol(), get_MESP(), get_GWP(),
+    return get_ethanol(), get_MESP(), get_GWP()
     
 
-def lignocellulose_county(name, state_data=STATE_DATA):
+def fermentation_county(name, state_data=STATE_DATA):
     """
     Take a county name from the user and return the annual ethanol price and GWP.
     
@@ -117,48 +216,115 @@ def lignocellulose_county(name, state_data=STATE_DATA):
         The data for the state.
         Set to default value of STATE_DATA.
         https://ecocomplex.rutgers.edu/biomass-energy-potential.html
+
+    Returns
+    -------
+    tuple
+        Contains the following:
+        name_final : str
+            The name of the county.
+        feedstock_kg_hr : int
+            Dry feedstock of lignocellulose in dry kg/hr.
+        ethanol : float
+            Annual ethanol in MM gal/year.
+        price : float
+            Price in $/gal.
+        gwp : float
+            GWP in lb CO2e/gal.
+        
+    Raises
+    ------
+    TypeError
+        If name is not a string.
+    TypeError
+        If state_data is not a pandas DataFrame.
+    ValueError
+        If the county name is not in the state data.
+        
     """
     # check if the name inputted in county exists in the first column of state_data
     
-    name_final = None
+    if not isinstance(name, str):
+        raise TypeError('County should be a string')
+    if not isinstance(state_data, pd.DataFrame):
+        raise TypeError('State data should be a pandas DataFrame')
     
-    for item in state_data['County']:
-        if item.lower() == name.lower():
-            name_final = item
-            break
-        
-    if name_final is None:
-        return None
-
-    # locate the 'Kilogram/hr' value for the specific county
-    dry_tonnes = int(state_data.loc[state_data['County'] == name_final, 'Lignocellulose (dry tons)'].values[0])
-    kg_per_hr = state_data.loc[state_data['County'] == name_final, 'Kilogram/hr'].values[0]
-
-    ethanol, price, gwp = lignocellulose_calc(kg_per_hr)
-
-    return name_final, dry_tonnes, round(ethanol, 3), round(price, 3), round(gwp, 3)
+    if name.lower() not in state_data['County'].str.lower().values:
+        raise ValueError(f"County '{name}' not found in the state data")
     
-def county_data_export_csv(name, df):
+    try:
+        name_final = state_data.loc[state_data['County'].str.lower() == name.lower(), 'County'].values[0]
+    except IndexError:
+        raise ValueError(f"County name '{name}' not found in the dataset.")
     
-    name_final = None
+    try:
+        dry_tonnes = int(state_data.loc[state_data['County'] == name_final, 'Lignocellulose (dry tons)'].values[0])
+    except KeyError:
+        raise KeyError("Column 'Lignocellulose (dry tons)' not found in the dataset.")
+    except ValueError:
+        raise ValueError(f"Value in 'Lignocellulose (dry tons)' for county '{name_final}' cannot be converted to an integer.")
     
-    for item in df['County']:
-        if item.lower() == name.lower():
-            name_final = item
-            break
-        
-    if name_final is None:
-        return None
+    try:
+        kg_per_hr = state_data.loc[state_data['County'] == name_final, 'Kilogram/hr'].values[0]
+    except KeyError:
+        raise KeyError("Column 'Kilogram/hr' not found in the dataset.")
+    except IndexError:
+        raise ValueError(f"Value for 'Kilogram/hr' not found for county '{name_final}'.")
 
-    # Return the data for the specific county in a pandas dataframe
-    return df.loc[df['County'] == name_final]
-
+    ethanol, price, gwp = fermentation_calc(kg_per_hr)
+    
+    return name_final, dry_tonnes, ethanol, price, gwp
+    
 if __name__ == '__main__':
     
-    df1 = pd.read_csv(r"backend\fermentation\biomass_imperial.csv")
-    df2 = pd.read_csv(r"backend\fermentation\biomass_metric.csv")
+    # Let's write some unit tests
     
-    print(lignocellulose_county('cape may'))
+    # Test 1 - Fermentation Convert Feedstock kg/hr
+    # Test 1 A - Test for valid inputs
+    print(fermentation_convert_feedstock_kg_hr(100, 'tons')) # 0.011363636363636364
+    print(fermentation_convert_feedstock_kg_hr(100, 'tonnes')) # 0.011363636363636364
+    print(fermentation_convert_feedstock_kg_hr(100, 'kghr')) # 100
     
-    print(county_data_export_csv('cape may', df1))
-    print(county_data_export_csv('cape may', df2))
+    # Test 1 B - Test for invalid inputs
+    try:
+        print(fermentation_convert_feedstock_kg_hr('100', 'tons')) # TypeError
+    except TypeError as e:
+        print(e)
+    try:
+        print(fermentation_convert_feedstock_kg_hr(100, 100)) # TypeError
+    except TypeError as e:
+        print(e)
+    try:
+        print(fermentation_convert_feedstock_kg_hr(100, 'ton')) # ValueError
+    except ValueError as e:
+        print(e)
+        
+    # Test 1 C - Test for edge cases
+        
+    # Test 2 - Lignocellulose Calc
+    # Test 2 A - Test for valid inputs
+    print(fermentation_calc(100)) # (0.0, 0.0, 0.0)
+
+    # Test 2 B - Test for invalid inputs
+    try:
+        print(fermentation_calc('100')) # TypeError
+    except TypeError as e:
+        print(e)
+    try:
+        print(fermentation_calc(100, '100')) # TypeError
+    except TypeError as e:
+        print(e)
+    try:
+        print(fermentation_calc(100, 0.2, 'GWP_CFs')) # TypeError
+    except TypeError as e:
+        print(e)
+        
+    # Test 3 - Fermentation County
+    # Test 3 A - Test for valid inputs        
+    print(fermentation_county('cape may')) # ('Cape May', 0, 0.0, 0.0, 0.0)
+    
+    # Test 3 B - Test for invalid inputs
+    try:
+        print(fermentation_county(100)) # TypeError
+    except TypeError as e:
+        print(e)
