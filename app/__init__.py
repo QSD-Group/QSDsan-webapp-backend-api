5eab10c2f19cf69ae5d191f6ea5b30e87a0a25b4
Naman Shah <80583973+navronaman@users.noreply.github.com>
2024-12-01 22:24:58 -0500

    Uncomment HTL blueprint registration in app initialization

--- app/__init__.py
+++ app/__init__.py
@@ -4,9 +4,9 @@ from flask_cors import CORS # flask-cors Version 5.0.0, we use this to allow cro
 from flasgger import Swagger # flasgger Version 0.9.7.1, we use this to generate swagger documentation for our API
 
 # We use blueprints from Flask to organize our API into different modules
-# from .blueprints.fermentation import fermentation_bp
-# from .blueprints.htl import htl_bp
-# from .blueprints.combustion import combustion_bp
+from .blueprints.fermentation import fermentation_bp
+from .blueprints.htl import htl_bp
+from .blueprints.combustion import combustion_bp
 # Also loading a trial blueprint to test stuff out
 from .blueprints.trial import trial_bp # Remove this line later
 
@@ -15,9 +15,9 @@ def create_app():
     CORS(app)
     Swagger(app)
     
-    # app.register_blueprint(fermentation_bp, url_prefix='/api/v1')
-    # app.register_blueprint(htl_bp, url_prefix='/api/v1')
-    # app.register_blueprint(combustion_bp, url_prefix='/api/v1')
+    app.register_blueprint(fermentation_bp, url_prefix='/api/v1')
+    app.register_blueprint(htl_bp, url_prefix='/api/v1')
+    app.register_blueprint(combustion_bp, url_prefix='/api/v1')
     
     # Also loading a trial blueprint to test stuff out
     app.register_blueprint(trial_bp, url_prefix='/api/v1') # Remove this line later
